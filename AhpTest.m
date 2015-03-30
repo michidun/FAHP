1093e75e7687854e23e1723475c1654b2a2adcc6
Michelle Dunmett <michidun@gmail.com>
2015-03-30 10:27:19 -0430

    Normalización fuzzy ahp estándar

--- AhpTest.m
+++ AhpTest.m
@@ -3,18 +3,23 @@ clf
 clear
 
 %%
-CompMat = [ 1     1     5     5     9     1/5    5
-            0     1     9     5     7     1/3    3
-            0     0     1     1/5   3     1/9    1
-            0     0     0     1     5     1/3    3
-            0     0     0     0     1     1/9    1
-            0     0     0     0     0      1     9
-            0     0     0     0     0      0     1];
+
+CompMat = [ 1     1/3   5     1/5   1     1/3   1/3   5     3
+		    0     1     9     1     5     3     3     7     5
+		    0     0     1     1/9   1/5   1/7   1/7   1/3   1/5
+		    0     0     0     1     5     3     3     9     7
+		    0     0     0     0     1     1/3   1/3   5     3
+		    0     0     0     0     0     1     1     7     3
+		    0     0     0     0     0     0     1     7     3
+		    0     0     0     0     0     0     0     1     1/3
+		    0     0     0     0     0     0     0     0     1 ];	
 
 
+printf("Matriz CompMat:\n");
 if ConsistencyAHP(CompMat)< 0.10
+	printf("Índice de consistencia: %f\n",ConsistencyAHP(CompMat))
     [result] = FuzzyAHP(CompMat);
-    disp(result);
+    result
     bar(result);
 end
 
--- FuzzyAHP.m
+++ FuzzyAHP.m
@@ -8,6 +8,7 @@ function [ result ] = FuzzyAHP( CompMat )
 %           October, 2011
 
 % fuzzy tfn and inverse fuzzy tfn constants
+% Escala de Tolga
 fuzzyTFN = {[1     1     1  ] 	[1      1    1  ]
             [1/2   3/4   1  ] 	[1      4/3  2  ]
             [2/3   1     3/2] 	[2/3    1    3/2]
@@ -17,6 +18,34 @@ fuzzyTFN = {[1     1     1  ] 	[1      1    1  ]
             [5/2   3     7/2] 	[2/7    1/3  2/5]
             [3     7/2   4  ] 	[1/4    2/7  1/3]
             [7/2   4     9/2] 	[2/9    1/4  2/7]};
+
+fuzzyTFN = {[1     1     1  ]   [1      1    1  ]
+            [1/2   1     3/2]   [2/3    1    2  ]
+            [1     3/2   2  ]   [1/2    2/3  1  ]
+            [3/2   2     5/2]   [2/5    1/2  2/3]
+            [2     5/2   3  ]   [1/3    2/5  1/2]
+            [5/2   3     7/2]   [2/7    1/3  2/5]};
+
+fuzzyTFN = {[1     1     3  ]   [1/3    1    1  ]
+            [1     2     4  ]   [1/4    1/2  1  ]
+            [1     3     5  ]   [1/5    1/3  1  ]
+            [2     4     6  ]   [1/6    1/4  1/2]
+            [3     5     7  ]   [1/7    1/5  1/3]
+            [4     6     8  ]   [1/8    1/6  1/4]
+            [5     7     9  ]   [1/9    1/7  1/5]
+            [6     8     9  ]   [1/9    1/8  1/6]
+            [7     9     9  ]   [1/9    1/9  1/7]};
+
+fuzzyTFN = {[1     1     1  ]   [1      1    1  ]
+            [1     2     3  ]   [1/3    1/2  1  ]
+            [2     3     4  ]   [1/4    1/3  1/2]
+            [3     4     5  ]   [1/5    1/4  1/3]
+            [4     5     6  ]   [1/6    1/5  1/4]
+            [5     6     7  ]   [1/7    1/6  1/5]
+            [6     7     8  ]   [1/8    1/7  1/6]
+            [7     8     9  ]   [1/9    1/8  1/7]
+            [8     9     10 ]   [1/10   1/9  1/8]};
+
 
 fuzzyCompMatCell={};
 
@@ -42,28 +71,44 @@ for i=1:m
     end
 end
 
-%%
-% find sum of every l,m,u values for triangular fuzzy number
+% Row sum
 for i=1:m
-    vec = [fuzzyCompMatCell{i,:}];
-    mExtendAnalysis{1,i} = sum(reshape(vec,3,[])');
+  RS(i,1) = 0;
+  RS(i,2) = 0;
+  RS(i,3) = 0;
+  for j=1:n
+    RS(i,1) = RS(i,1) + fuzzyCompMatCell{i,j}(1);
+    RS(i,2) = RS(i,2) + fuzzyCompMatCell{i,j}(2);
+    RS(i,3) = RS(i,3) + fuzzyCompMatCell{i,j}(3);
+  end
 end
 
-vec = [mExtendAnalysis{1,:}];
-mExtendAnalysisSum = sum(reshape(vec,3,[])');
-
-for i=1:m
-    vec = [mExtendAnalysis{1,i}];
-    for j=1:3
-        val = mExtendAnalysisSum(1,j);
-        %valSum(1,j) = val*vec(1,j);
-        valSum(1,j) = (vec(1,j))*(1/val);
-    end
-    mExtendAnalysis{1,i} = valSum;
+%Column sum
+for i=1:n
+  CS(i,1) = 0;
+  CS(i,2) = 0;
+  CS(i,3) = 0;
+  for j=1:m
+    CS(i,1) = CS(i,1) + fuzzyCompMatCell{j,i}(1);
+    CS(i,2) = CS(i,2) + fuzzyCompMatCell{j,i}(2);
+    CS(i,3) = CS(i,3) + fuzzyCompMatCell{j,i}(3);
+  end
 end
 
-for i=1:m
-    result(i) = sum(mExtendAnalysis{i})/3;
-end
+NM_MAT = RS / 9;
+NM_SUM =  sum(NM_MAT);
 
+N = [NM_MAT(:,1)/NM_SUM(3), NM_MAT(:,2)/NM_SUM(2), NM_MAT(:,3)/NM_SUM(1)];
+N = reshape(N',1,27);
+
+IN = fliplr(CS);
+IN = reshape(IN',1,27);
+IN = IN.^-1;
+
+AVP = (IN+N)/2;
+
+%Dezfucificación con centroide
+crisp = sum(reshape(AVP, 3, 9)',2)/3;
+
+result = crisp;
 end
