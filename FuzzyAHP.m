ddcf54a26f8185a3a4b203bdc49fea2f7c76c373
Michelle Dunmett <michidun@gmail.com>
2015-04-09 17:40:28 -0430

    método geomean

--- FuzzyAHP.m
+++ FuzzyAHP.m
@@ -13,42 +13,28 @@ function [ result ] = FuzzyAHP( fuzzyCompMatCell )
 
 % Row sum
 for i=1:m
-  RS(i,1) = 0;
-  RS(i,2) = 0;
-  RS(i,3) = 0;
+  clear lv mv uv;
   for j=1:n
-    RS(i,1) = RS(i,1) + fuzzyCompMatCell{i,j}(1);
-    RS(i,2) = RS(i,2) + fuzzyCompMatCell{i,j}(2);
-    RS(i,3) = RS(i,3) + fuzzyCompMatCell{i,j}(3);
+    lv(end+1) = fuzzyCompMatCell{i,j}(1);
+    mv(end+1) = fuzzyCompMatCell{i,j}(2);
+    uv(end+1) = fuzzyCompMatCell{i,j}(3);
   end
+  lm(i) = geomean(lv);
+  mm(i) = geomean(mv);
+  um(i) = geomean(uv);
 end
 
-%Column sum
-for i=1:n
-  CS(i,1) = 0;
-  CS(i,2) = 0;
-  CS(i,3) = 0;
-  for j=1:m
-    CS(i,1) = CS(i,1) + fuzzyCompMatCell{j,i}(1);
-    CS(i,2) = CS(i,2) + fuzzyCompMatCell{j,i}(2);
-    CS(i,3) = CS(i,3) + fuzzyCompMatCell{j,i}(3);
-  end
-end
+NM_MAT = vertcat(lm, mm, um)';
+NM_MAT
 
-NM_MAT = RS / 9;
 NM_SUM =  sum(NM_MAT);
 
 N = [NM_MAT(:,1)/NM_SUM(3), NM_MAT(:,2)/NM_SUM(2), NM_MAT(:,3)/NM_SUM(1)];
-N = reshape(N',1,m*3);
+N
 
-IN = fliplr(CS);
-IN = reshape(IN',1,m*3);
-IN = IN.^-1;
-
-AVP = (IN+N)/2;
-
-for i=0:m-1
-  result{i+1} = [AVP(i*3+1), AVP(i*3+2), AVP(i*3+3)];
+for i=1:m
+  result{i} = [N(i,1), N(i,2), N(i,3)];
 end
 
 end
+ 
